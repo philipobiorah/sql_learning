24d2fd7838c67ca5b784a1f16527179cb0eb6e1c
PHILIP OBIORAH <philip.obiorah@outlook.com>
2024-03-18 23:26:49 +0000

    creating comments

--- sql_learning.sql
+++ sql_learning.sql
@@ -3,5 +3,25 @@
 /*
 
 a comment block in sql
+CREATED BY: PHILIP OBIORAH
+CREATE DATE: 17/03/2024
+DESCRIPTION: THIS QUERY DISPLAY ALL CUSTOMERS FIRST, LAST NAMES AND EMAIL ADDRESS
 
 */
+
+
+
+/*
+
+Created By: Philip Obiorah
+Create Date: 16/03/2024
+Discription: this query displays all customers first, LastName, and Email Address
+*/
+
+
+SELECT 
+	FirstName, 
+	LastName,
+	Email
+FROM
+	Customer
