470ce606e0d98f1491467de68ddda4c259d95aab
PHILIP OBIORAH <philip.obiorah@outlook.com>
2024-03-19 19:56:25 +0000

    using case

--- sql_learning.sql
+++ sql_learning.sql
@@ -117,3 +117,106 @@ WHERE
 	BillingCity  LIKE 'B%'
 ORDER BY 
 	InvoiceDate
+
+
+-- How many invoices were billed on 2010-05-22 00:00:00
+SELECT
+	InvoiceDate
+	BillingAddress,
+	BillingCity,
+	total
+ 
+FROM 
+	Invoice
+WHERE 
+	InvoiceDate = '2010-05-22 00:00:00'
+ORDER BY 
+	InvoiceDate
+
+
+
+-- How many invoices were billed on 2010-05-22 00:00:00
+SELECT
+	InvoiceDate
+	BillingAddress,
+	BillingCity,
+	total
+ 
+FROM 
+	Invoice
+WHERE 
+	--USING THE DATE FUNCTION
+	DATE(InvoiceDate ) = '2010-05-22'
+ORDER BY 
+	InvoiceDate
+
+-- How many invoices were billed after 2010-05-22 and have totla of less than 3.00
+SELECT
+	InvoiceDate
+	BillingAddress,
+	BillingCity,
+	total
+ 
+FROM 
+	Invoice
+WHERE 
+	DATE(InvoiceDate ) > '2010-05-22' AND total < 3.00
+ORDER BY 
+	InvoiceDate
+	
+
+--Get all invoice who's billing city is starrts with P or starts with D
+SELECT
+	InvoiceDate
+	BillingAddress,
+	BillingCity,
+	total
+FROM 
+	Invoice
+WHERE
+	BillingCity LIKE 'P%' OR BillingCity LIKE 'D%'
+
+--Get all invoices that are greater than 1.98 from any cities whose name start with P or starts with D?
+SELECT
+	InvoiceDate
+	BillingAddress,
+	BillingCity,
+	total
+FROM 
+	Invoice
+WHERE
+	total > 1.98
+	AND
+	(BillingCity LIKE 'P%' OR BillingCity LIKE 'D%')
+
+--ORDER OF OPERAITON
+--BEMDAS- Brackets, Exponents, multiplication/division, additon/sub
+
+
+
+/*
+WSDA Music Salses Goal:
+They want as many customers as possible to  spend
+between $7.00 and $15.00
+
+Sales categories:
+Baseline Purchase - between $0.99 and $1.99
+Low Purchase - Between $2.00 and $6.99
+Target Purchase -  Between $7.00 and $15.00
+Top Performer - Above $15.00
+*/
+SELECT
+	InvoiceDate,
+	BillingAddress,
+	BillingCity,
+	total,
+	CASE 
+	WHEN total < 2.00 THEN 'Baseline Purchase'
+	WHEN total BETWEEN 2.00  AND 6.99 THEN 'low Purchase'
+	WHEN total BETWEEN 7.00 AND 15.00 THEN 'Target Purchase'
+	ELSE 'Top Performer'
+	END AS PurchaseType
+FROM 
+	Invoice
+ORDER BY
+	BillingCity
