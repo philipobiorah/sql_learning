677c2d2b7fe230073abfad368db5fa9cbe87e8a2
PHILIP OBIORAH <philip.obiorah@outlook.com>
2024-04-05 23:47:21 +0100

    get current second

--- showcurrentTime.py
+++ showcurrentTime.py
@@ -4,3 +4,9 @@ currentTime = time.time() #Get current time
 
 #Obtain the total seconds since midnight Jan 1, 1970
 
+totalSeconds = int(currentTime)
+
+
+#Get the current second
+currentSecond = totalSeconds % 60
+
