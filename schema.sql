6fa251921a3940fbfaac9fa8b8414716b5a38d5d
PHILIP OBIORAH <philip.obiorah@outlook.com>
2024-07-02 15:52:10 +0100

    create table for universities

--- schema.sql
+++ schema.sql
@@ -11,3 +11,36 @@ WHERE table_schema = 'public'
 SELECT column_name, data_type 
 FROM information_schema.columns
 WHERE table_name = 'university_professors' AND table_schema = 'public';
+
+-- Create table for professors
+CREATE TABLE professors(
+    firstname text,
+    lastname text
+);
+
+
+-- Create a table for the professors entity type
+CREATE TABLE professors (
+ firstname text,
+ lastname text
+);
+
+-- Print the contents of this table
+SELECT * 
+FROM professors
+
+
+-- Create a table for the universities entity type
+CREATE Table universities (
+    university_shortname text,
+    university text,
+    university_city text
+);
+
+
+
+
+
+-- Print the contents of this table
+SELECT * 
+FROM universities
