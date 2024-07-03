35e91c7a6810121404b643d0a5fb11fe73045bc6
PHILIP OBIORAH <philip.obiorah@outlook.com>
2024-07-03 09:55:19 +0100

    convert the values of the first name to max of 16 chars

--- schema.sql
+++ schema.sql
@@ -89,3 +89,9 @@ FROM transactions;
 ALTER TABLE professors
 ALTER COLUMN firstname
 TYPE varchar(64);
+
+-- Convert the values in firstname to a max. of 16 characters
+ALTER TABLE professors 
+ALTER COLUMN firstname 
+TYPE varchar(16)
+USING SUBSTRING(firstname FROM 1 FOR 16)
