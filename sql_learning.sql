f20c3f34409dea554dbe958360e2c07b7c84ab84
PHILIP OBIORAH <philip.obiorah@outlook.com>
2024-08-20 09:10:43 +0100

    creating dimension table

--- sql_learning.sql
+++ sql_learning.sql
@@ -847,3 +847,19 @@ CREATE TABLE affiliations (
 	organization_id varchar(256) REFERENCES organizations (id),
 	function varchar(256)
 );
+
+-- Create a route dimension table
+CREATE TABLE route(
+	route_id INTEGER PRIMARY KEY,
+    route_name VARCHAR(160) NOT NULL,
+    city_name VARCHAR(160) NOT NULL,
+    distance_km float NOT NULL,
+    park_name VARCHAR(160) NOT NULL
+);
+-- Create a week dimension table
+CREATE TABLE week(
+	week_id INTEGER PRIMARY KEY,
+    week INTEGER NOT NULL,
+    month VARCHAR(160) NOT NULL,
+    year INTEGER NOT NULL
+);
